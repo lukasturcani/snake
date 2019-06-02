ec8d323de9354d54e7d54b2e9cd62655700699ca
Lukas Turcani <lukasturcani93@gmail.com>
2019-06-03 00:43:58 +0100

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
 
-setup(name='snake',
-      author='Lukas Turcani',
-      author_email='lukasturcani93@gmail.com',
-      url='https://www.github.com/lukasturcani/snake',
-      version='2019.06.03',
-      packages=['snake', 'snake.game', 'snake.game_io'],
-      python_requires='>=3.6')
+setup(
+    name='snake',
+    author='Lukas Turcani',
+    author_email='lukasturcani93@gmail.com',
+    url='https://www.github.com/lukasturcani/snake',
+    version='2019.06.03',
+    packages=['snake'],
+    python_requires='>=3.6'
+)
